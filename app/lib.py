97c6bd1adc35270641cffa6e76abeb71e1df5dcb
Gabriel Noronha Pereira dos Santos <gnpsantos@rd.com.br>
2023-12-26 15:08:13 -0300

    added multiple parties to be managed at once

--- app/lib.py
+++ app/lib.py
@@ -1,9 +1,11 @@
-import pandas as pd
+import configparser as cfg
 import json
 
+import pandas as pd
 
-PESSOAS_DB = "convidados.json"
-ITEMS_DB = "items.csv"
+
+PESSOAS_DB = "parties/{token}/convidados.json"
+ITEMS_DB = "parties/{token}/items.csv"
 
 CODE_TO_PROD_NAME = {
     "cerveja": "Cerveja",
@@ -13,7 +15,7 @@ CODE_TO_PROD_NAME = {
 }
 
 
-def add_person(name, checklist):
+def add_person(name, checklist, token):
     '''
     {
     "cerveja":True,
@@ -28,18 +30,18 @@ def add_person(name, checklist):
     open(PESSOAS_DB, 'w').write(json.dumps(d))
     return True
 
-def load_person(name):
+def load_person(name, token):
     name = name.lower().strip()
-    d = json.loads(open(PESSOAS_DB).read())
+    d = json.loads(open(PESSOAS_DB.replace('{token}',token)).read())
     person_data = d.get(name)
     if person_data:
         return True, person_data
     else:
         return False, {}
 
-def add_item(owner, product_name, quantity, category, unit_price):
+def add_item(owner, product_name, quantity, category, unit_price, token):
     # Checking if the owner exists
-    if not load_person(owner):
+    if not load_person(owner, token):
         return False, "Cadastro não encontrado"
     # Checking if the quantity is numeric
     try:
@@ -53,85 +55,88 @@ def add_item(owner, product_name, quantity, category, unit_price):
         return False, "Confira o valor 'valor unitario'"
     #
     owner = owner.lower().strip()
-    df = get_items()
+    df = get_items(token)
     new_data = {"responsavel": [owner], "produto": [product_name], "quantidade": [quantity], "categoria": [category], "preco_unit": [unit_price]}
     df_new_data = pd.DataFrame(new_data)
     df_new = pd.concat([df, df_new_data], ignore_index=True)
-    df_new.to_csv(ITEMS_DB, index=False)
+    save_items(df_new, token)
     return True, ""
 
-def get_product_category(product_name):
-    df = get_items()
+def get_product_category(product_name, token):
+    df = get_items(token)
     return df[df['produto']==product_name]['categoria'].values[0]
 
-def list_users():
-    return list(json.loads(open(PESSOAS_DB).read()).keys())
+def list_users(token):
+    return list(json.loads(open(PESSOAS_DB.replace('{token}',token)).read()).keys())
 
-def list_products():
-    df = get_items()
+def list_products(token):
+    df = get_items(token)
     return list(df['produto'].values)
 
-def get_user_categories(user):
-    r, user_data = load_person(user)
+def get_user_categories(user, token):
+    r, user_data = load_person(user, token)
     return [k for k,v in user_data.items() if v]
 
-def get_consumers_for_product(product_name):
-    category = get_product_category(product_name)
-    users = list_users()
+def get_consumers_for_product(product_name, token):
+    category = get_product_category(product_name, token)
+    users = list_users(token)
     consumers = []
     for user in users:
-        selected_categories = get_user_categories(user)
+        selected_categories = get_user_categories(user, token)
         selected_categories = [CODE_TO_PROD_NAME[c] for c in selected_categories]
         if category in selected_categories:
             consumers.append(user)
     return consumers
 
-def get_product_price(product_name):
-    df = get_items()
+def get_product_price(product_name, token):
+    df = get_items(token)
     unit_price = df[df['produto']==product_name]['preco_unit'].iloc[0]
     quant = df[df['produto']==product_name]['quantidade'].iloc[0]
     total = unit_price * quant
     return unit_price, quant, total
 
-def get_products_for_user(user):
+def get_products_for_user(user, token):
     prod_list = []
-    for prod in list_products():
-        if user in get_consumers_for_product(prod):
+    for prod in list_products(token):
+        if user in get_consumers_for_product(prod, token):
             prod_list.append(prod)
     return prod_list
 
-def get_amount_for_user(user):
+def get_amount_for_user(user, token):
     total_value = 0
     # products this user consumes
-    products = get_products_for_user(user)
+    products = get_products_for_user(user, token)
     # for each product, its fraction value is
     for prod in products:
-        unit_price, quant, total = get_product_price(prod)
-        consumers = get_consumers_for_product(prod)
+        unit_price, quant, total = get_product_price(prod, token)
+        consumers = get_consumers_for_product(prod, token)
         if user in consumers:
             price_per_consumer = total/len(consumers)
             total_value += price_per_consumer
     return total_value
 
-def remove_product(df, quant):
+def remove_product(df, quant, token):
     quant = int(quant)
     prod_name = df['produto'].iloc[0]
     prod_quant = df['quantidade'].iloc[0]
     max_rem = min(quant, prod_quant)
     new_quant = prod_quant - max_rem
-    df = get_items()
+    df = get_items(token)
     if new_quant == 0:
         df = df[df['produto']!=prod_name]
-        save_items(df)
+        save_items(df, token)
     else:
         df.loc[df['produto'] == prod_name, 'quantidade'] = new_quant 
     return prod_name, max_rem
         
-def get_items():
-    df = pd.read_csv(ITEMS_DB)
-    if 'Unnamed: 0' in df.columns:
-        df = df.drop('Unnamed: 0', axis=1)
+def get_items(token):
+    filename = ITEMS_DB.replace('{token}',token)
+    df = pd.read_csv(filename)
+    cols = [c for c in df.columns if 'nnamed' in c]
+    for c in cols:
+        df.drop(columns=c, inplace=True)
     return df
 
-def save_items(df):
-    df.to_csv(ITEMS_DB)
+def save_items(df, token):
+    filename = ITEMS_DB.replace('{token}',token)
+    df.to_csv(filename, index=False)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+import configparser as cfg
+import os
+
 import streamlit as st
+
 import lib
 
 
@@ -11,17 +15,11 @@ remap_item = {
 
 
 # Function for the Subscription Page
-def subscription_page():
+def subscription_page(token):
     st.title("Inscrição")
 
     # Text input for name
     nome = st.text_input("Nome:")
-    token = st.text_input("token (veja no grupo): ")
-
-    if token != "galeradalaje":
-        st.error("token invalido, pergunte para (98)98270-0001")
-    else:
-        st.success("token válido")
 
     # Checkboxes for various consumable items
     st.write("O que vai consumir:")
@@ -32,12 +30,12 @@ def subscription_page():
 
     if st.button("Salvar"):
         d = {"cerveja": cerveja, "cachaca": cachaca, "salgados_e_doces": salgados_doces, "refri_e_suco": salgados_doces}
-        lib.add_person(nome, d)
+        lib.add_person(nome, d, token)
         st.success("Dados salvos com sucesso!")
         
 
 # Function for the Subscription Status Page
-def subscription_status_page():
+def subscription_status_page(token):
     st.title("Seus dados:")
 
     nome = st.text_input("Nome cadastrado:")
@@ -45,7 +43,7 @@ def subscription_status_page():
 
     if st.button("Carregar Dados"):
         # Fetching data using the load_person function
-        r, preferences = lib.load_person(nome)
+        r, preferences = lib.load_person(nome, token)
 
         if r:
             st.write(f"Consumos:")
@@ -56,9 +54,9 @@ def subscription_status_page():
         else:
             st.warning("Nome não encontrado.")
 
-        categorias = [remap_item[v] for v,k in lib.load_person(nome)[1].items() if k]
+        categorias = [remap_item[v] for v,k in lib.load_person(nome, token)[1].items() if k]
 
-        df = lib.get_items()
+        df = lib.get_items(token)
         df['total'] = df['preco_unit'] * df['quantidade']
         df = df[df['categoria'].isin(categorias)]
         st.title("Contribuições:")
@@ -72,20 +70,25 @@ def subscription_status_page():
         st.dataframe(available_df)
         st.title("Valor total da parte:")
         #available_df['total'] = available_df['preco_unit'] * available_df['quantidade']
-        total_value = lib.get_amount_for_user(nome)
+        total_value = lib.get_amount_for_user(nome, token)
         st.title(f"R$ {total_value:.2f}".replace('.',','))
         st.title("Valor a ser pago (total - gastos):")
         st.title(f"R$ {total_value-own_total_value:.2f}")
         st.title("Pix:")
-        st.image("pix.jpeg")
+        st.image(f"parties/{token}/pix.jpeg")
+        config = cfg.ConfigParser()
+        config.read(f'parties/{token}/info.conf')
+        pix_key = config['DEFAULT']['pix_key']
+        st.write("chave pix:")
+        st.write(pix_key)
 
 # Function for the Add Items Page
-def add_items_page():
+def add_items_page(token):
     st.title("Adicione produtos que você vai levar:")
 
     nome = st.text_input("Nome:")
     if st.button("Verificar"):
-        r, preferences = lib.load_person(nome)
+        r, preferences = lib.load_person(nome, token)
         if r:
             st.success("cadastro encontrado")
         else:
@@ -95,24 +98,24 @@ def add_items_page():
     category = st.selectbox('Categoria: ', ["Cerveja", "Cachaça", "Salgados e Doces", "Refri e Suco"])
     unit_value = st.text_input("Valor unitário")
     if st.button("Inserir"):
-        r, msg = lib.add_item(nome, prod_name, quant, category, unit_value)
+        r, msg = lib.add_item(nome, prod_name, quant, category, unit_value, token)
         if not r:
             st.error(msg)
         else:
             st.success("Produto adicionado com sucesso")
 
-def check_items_page():
+def check_items_page(token):
+    df = lib.get_items(token)
     st.title("Itens da festa:")
-    st.dataframe(lib.get_items())
+    st.dataframe(df)
     st.title("Valor total:")
-    df = lib.get_items()
     df['total'] = df['preco_unit'] * df['quantidade']
     total_val = sum(df['total'])
     st.title(f"R$ {total_val:.2f}".replace(".",","))
 
-def remove_items_page():
+def remove_items_page(token):
     nome = st.text_input("Nome cadastrado:")
-    df = lib.get_items()
+    df = lib.get_items(token)
     items = df[df['responsavel']==nome]['produto'].values
     selecao = st.selectbox("Item a ser removido:", items)
     quant = st.text_input("Quantidade:")
@@ -120,24 +123,34 @@ def remove_items_page():
     selected_df = df_u[df_u['produto']==selecao]
     st.dataframe(selected_df)
     if st.button("remover"):
-        prod_name, units = lib.remove_product(selected_df, quant)
+        prod_name, units = lib.remove_product(selected_df, quant, token)
         st.success(f"Retirado {units} unidades do produto {prod_name}")
 
 
 def main():
-    st.sidebar.title("Navegação")
-    page = st.sidebar.radio("Go to", ("Inscrição", "Conta", "Adicionar Item", "Remover Item", "Verificar Itens"))
+    available_tokens = os.listdir('parties')
+    token = st.sidebar.text_input("token (veja no grupo): ")
+    valid_token = token in available_tokens
 
-    if page == "Inscrição":
-        subscription_page()
-    elif page == "Conta":
-        subscription_status_page()
-    elif page == "Adicionar Item":
-        add_items_page()
-    elif page == "Remover Item":
-        remove_items_page()
-    elif page == "Verificar Itens":
-        check_items_page()
+    if not valid_token:
+        st.sidebar.error(f"token invalido, pergunte para (98)98270-0001")
+    else:
+        st.sidebar.success("token válido")
+
+    if valid_token:
+        st.sidebar.title("Navegação")
+        page = st.sidebar.radio("Go to", ("Inscrição", "Conta", "Adicionar Item", "Remover Item", "Verificar Itens"))
+
+        if page == "Inscrição":
+            subscription_page(token)
+        elif page == "Conta":
+            subscription_status_page(token)
+        elif page == "Adicionar Item":
+            add_items_page(token)
+        elif page == "Remover Item":
+            remove_items_page(token)
+        elif page == "Verificar Itens":
+            check_items_page(token)
 
 if __name__ == "__main__":
     main()
